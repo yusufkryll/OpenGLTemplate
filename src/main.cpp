904cc6be07b65f7d4d484b0cbe48ab55aefe48aa
JSDevloop <56578188+JSDevloop@users.noreply.github.com>
2023-07-01 13:37:02 +0300

    first

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	if (!glfwInit())
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(800,600, "FirstProgram", NULL, NULL); // pencere burada oluşturulur.
+
+	if (window == NULL) // window oluşturulamadığı durumlar olabilir bu durumlarda gli terminate etmek gerekir.
+	{
+		cout << "Window cannot instantiated.";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window); //OpenGl ile yaptığımız işlemlerin hangi pencerede işleneceğini belirttiğimiz bir metod.
+
+	while (!glfwWindowShouldClose(window)) // Pencere kapanmadığı durumlarda bu while döngüsü içinde bulunmamız gerekir.
+	{
+		glfwSwapBuffers(window); //OpenGl ile yapılan çizimlerin pencereye aktarılması.
+		glfwPollEvents(); //Pencerede yapılan eventleri yakalamamızı sağlayan fonksiyon.
+	}
+
+	cout << "Program terminated." << endl; // Eğer pencere kapatılırsa çalıştırılır.
+
+	return 0;
+}
